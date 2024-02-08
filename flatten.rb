6442558bbe8f856ead547ef05d49607f57d37222
Eric Belongea <eric.belongea@gmail.com>
2024-02-08 09:52:18 -0700

    Creates a custom flatten method

---
+++ flatten.rb
@@ -0,0 +1,29 @@
+class Array
+  def custom_flatten
+    result = []
+
+    each do |element|
+      if element.is_a?(Array)
+        result.concat(element.custom_flatten)
+      else
+        result << element
+      end
+    end
+
+    result
+  end
+end
+
+nested_array = [1, [2, [3, 4], 5], 6]
+flattened_array = nested_array.custom_flatten
+puts flattened_array.inspect
+
+mega_nested = [[1,2], [3,[4,5],6]]
+flat2 = mega_nested.custom_flatten
+puts flat2.inspect
+
+another_nest = [[1,2],[3,[4,5]], 6]
+flat3 = another_nest.custom_flatten
+puts flat3.inspect
+
+
