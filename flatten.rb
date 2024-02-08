b0634e200f089002823eed4790f8792d8f1b5d4e
Eric Belongea <eric.belongea@gmail.com>
2024-02-08 10:28:57 -0700

    Shows how concat works

--- flatten.rb
+++ flatten.rb
@@ -16,14 +16,22 @@ end
 
 nested_array = [1, [2, [3, 4], 5], 6]
 flattened_array = nested_array.custom_flatten
-puts flattened_array.inspect
+# puts flattened_array.inspect
 
 mega_nested = [[1,2], [3,[4,5],6]]
 flat2 = mega_nested.custom_flatten
-puts flat2.inspect
+# puts flat2.inspect
 
 another_nest = [[1,2],[3,[4,5]], 6]
 flat3 = another_nest.custom_flatten
-puts flat3.inspect
+# puts flat3.inspect
 
 
+## Showing how Concat works.
+
+arr1 = [1,2,3]
+arr2 = [4,5,6]
+
+ca = arr1.concat(arr2)
+
+# puts ca.inspect
