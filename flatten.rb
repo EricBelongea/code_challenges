ab2bde7ac6662cfaed7a7c363a2acc2d3ca7152b
Eric Belongea <eric.belongea@gmail.com>
2024-02-08 10:54:23 -0700

    Tweaking for more nesting

--- flatten.rb
+++ flatten.rb
@@ -16,15 +16,15 @@ end
 
 nested_array = [1, [2, [3, 4], 5], 6]
 flattened_array = nested_array.custom_flatten
-# puts flattened_array.inspect
+puts flattened_array.inspect
 
 mega_nested = [[1,2], [3,[4,5],6]]
 flat2 = mega_nested.custom_flatten
-# puts flat2.inspect
+puts flat2.inspect
 
-another_nest = [[1,2],[3,[4,5]], 6]
+another_nest = [[1,2],[3,[4,5]],[6]]
 flat3 = another_nest.custom_flatten
-# puts flat3.inspect
+puts flat3.inspect
 
 
 ## Showing how Concat works.
@@ -34,4 +34,4 @@ arr2 = [4,5,6]
 
 ca = arr1.concat(arr2)
 
-# puts ca.inspect
+puts ca.inspect
