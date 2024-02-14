40e813f562528e85611fa68d5357fdfc920ffae6
Eric Belongea <eric.belongea@gmail.com>
2024-02-14 10:27:45 -0700

    Who likes this?
    I do

---
+++ codewars/grasshopper_combine_strings.rb
@@ -0,0 +1,13 @@
+# Combine strings function
+# Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.
+
+# Example:
+
+# combine_names('James', 'Stevens')
+# returns:
+
+# 'James Stevens'
+
+def combine_names(first, last)
+  "#{first} #{last}"
+end
